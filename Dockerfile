1282ee17a0da028b0a3db66054a6ddb3d22ee8ba
msrocka <michael.srocka@gmail.com>
2023-08-07 14:11:27 +0200

    native libs & cleanup

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,12 @@
-# Collect Maven dependencies
-FROM maven:3.9.3-eclipse-temurin-17
+# collect Maven dependencies
+FROM maven:3.9.3-eclipse-temurin-17 AS mvn
 WORKDIR /olca-ipc
 COPY pom.xml .
 RUN mvn package
+
+# native libraries
+FROM ghcr.io/greendelta/gdt-server-native AS native
+
+FROM eclipse-temurin:17-jre
+COPY --from=mvn /olca-ipc/target/lib /app/lib
+COPY --from=native /app/native /app/native
--- scripts/build.dart
+++ scripts/build.dart
@@ -10,15 +10,65 @@ main(List<String> args) {
       case "build":
         _build();
         break;
+      case "clean":
+        _clean();
+        break;
       default:
         print("error: unknown command: ${args[0]}");
     }
   }
 }
 
-void _build() {
-  print("build image $IMAGE ...");
+_build() {
+  _clean();
+  print("  build image $IMAGE");
   _docker(["build", "-t", IMAGE, "."]);
+}
+
+_clean() {
+  // stop a possible running container
+  _eachLineOf(_docker(["ps"]), (line) {
+    if (line.length < 2) return;
+    final container = line.last.trim();
+    final image = line[1].trim();
+    if (image == IMAGE) {
+      print("  stop container $container (image: $image)");
+      _docker(["stop", container]);
+    }
+  });
+
+  // delete a stopped container
+  _eachLineOf(_docker(["ps", "-a"]), (line) {
+    if (line.length < 2) return;
+    var containerId = line[0].trim();
+    var image = line[1].trim();
+    if (image == IMAGE) {
+      print("  delete container ${containerId} (image: ${image})");
+      _docker(["rm", containerId]);
+    }
+  });
+
+  // delete image
+  _eachLineOf(_docker(["image", "ls"]), (line) {
+    if (line.length < 2) return;
+    var image = line[0].trim();
+    if (image == IMAGE) {
+      print("  delete image ${image}");
+      _docker(["image", "rm", image]);
+    }
+  });
+}
+
+_eachLineOf(ProcessResult pr, Function(List<String>) fn) {
+  final out = pr.stdout;
+  if (out == null) {
+    return;
+  }
+  final ws = RegExp("\\s+");
+  for (var line in out.toString().split("\n")) {
+    var parts = line.trim().split(ws);
+    fn(parts);
+  }
 }
 
 ProcessResult _docker(List<String> args) {
